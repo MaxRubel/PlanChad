55c2ebf8819cb5eb80bb74456372a1e060cbcd16
Max Rubel <maxrubel1@gmail.com>
2024-03-20 18:43:14 -0500

    fixed small typo delete proj modal

--- components/modals/DeleteProject.js
+++ components/modals/DeleteProject.js
@@ -7,7 +7,7 @@ export default function DeleteProjectModal({ show, closeModal, handleDelete }) {
     <>
       <Modal show={show} onHide={closeModal}>
         <Modal.Header>
-          <Modal.Title>Delete Task</Modal.Title>
+          <Modal.Title>Delete Project</Modal.Title>
         </Modal.Header>
         <Modal.Body>Are you sure you want to delete this project?  This action cannot be undone.</Modal.Body>
         <Modal.Footer>
